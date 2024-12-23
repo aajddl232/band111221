6cf7f45f9405911259965d1c9bbff7ec395d4288
aajddl232 <rowktlr3368@gmail.com>
2024-12-23 10:15:06 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,14 @@
+$(function () {
+    $('.main_visual_report').slick({
+        arrows: false,
+    });
+
+
+    $('.main_visual_report').slick({
+        arrows: false,
+        slidesToShow: 3,
+        dots: true,
+
+    });
+
+})
